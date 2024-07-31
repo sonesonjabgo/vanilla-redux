625ed473feb08335c88ca0a28aa82f57db3c016b
sonesonjabgo <thswogud02@gmail.com>
2024-07-31 13:53:32 +0900

    feat: add handler

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  console.log(count);
-  return count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else return count;
 };
 
 const countStore = createStore(countModifier);
 
-console.log(countStore.getState());
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
